c74af33335bee711d179f6e5f3adebbbba91bdec
Milosh Zelembaba <mzelemba@uwaterloo.ca>
2018-07-03 10:01:10 -0400

    wip

--- Play/app/src/main/java/com/miloshzelembaba/play/Spotify/SpotifyManager.java
+++ Play/app/src/main/java/com/miloshzelembaba/play/Spotify/SpotifyManager.java
@@ -1,11 +1,36 @@
 package com.miloshzelembaba.play.Spotify;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaBrowserServiceCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v4.media.app.NotificationCompat.MediaStyle;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.miloshzelembaba.play.Activity.StartActivity.InitialActivity;
 import com.miloshzelembaba.play.Models.Song;
 import com.miloshzelembaba.play.Models.User;
+import com.miloshzelembaba.play.R;
 import com.miloshzelembaba.play.Utils.ApplicationUtil;
 import com.miloshzelembaba.play.api.Services.RefreshSpotifyAccessTokenService;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
@@ -18,6 +43,7 @@ import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -34,7 +60,7 @@ import retrofit.RetrofitError;
  * Created by miloshzelembaba on 2018-03-25.
  */
 
-public class SpotifyManager implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+public class SpotifyManager extends MediaBrowserServiceCompat implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback, MediaPlayer.OnCompletionListener, AudioManager.OnAudioFocusChangeListener {
     public static final String CLIENT_ID = "75cc7c4b4c6d49388044414a5ba6aaa6";
     public static final String REDIRECT_URI = "what://localhost:8888/callback";
     public static final int REQUEST_CODE = 1337;
@@ -47,6 +73,77 @@ public class SpotifyManager implements SpotifyPlayer.NotificationCallback, Conne
     static SpotifyApi mSpotifyApi;
     static SpotifyPlayer mSpotifyPlayer;
     static SpotifyManager instance;
+    private static String mCurrentUri;
+    private int mCurrentState;
+
+    // Media services
+    private MediaPlayer mMediaPlayer;
+    private MediaSessionCompat mMediaSessionCompat;
+    private BroadcastReceiver mNoisyReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if( mMediaPlayer != null && mMediaPlayer.isPlaying() ) {
+                mMediaPlayer.pause();
+            }
+        }
+    };
+    private MediaSessionCompat.Callback mMediaSessionCallback = new MediaSessionCompat.Callback() {
+
+        @Override
+        public void onPlay() {
+            super.onPlay();
+
+            if( !successfullyRetrievedAudioFocus() ) {
+                return;
+            }
+
+            mMediaSessionCompat.setActive(true);
+            setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING);
+            showPlayingNotification();
+            mMediaPlayer.start();
+//            if (getPlayer() != null) {
+//                getPlayer().resume(null);
+//            }
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+
+            if( mMediaPlayer.isPlaying() ) {
+                mMediaPlayer.pause();
+                setMediaPlaybackState(PlaybackStateCompat.STATE_PAUSED);
+                showPausedNotification();
+            }
+        }
+
+        @Override
+        public void onPlayFromMediaId(String mediaId, Bundle extras) {
+            super.onPlayFromMediaId(mediaId, extras);
+            initMediaSessionMetadata();
+        }
+    };
+    private MediaControllerCompat.Callback mMediaControllerCompatCallback = new MediaControllerCompat.Callback() {
+
+        @Override
+        public void onPlaybackStateChanged(PlaybackStateCompat state) {
+            super.onPlaybackStateChanged(state);
+            if( state == null ) {
+                return;
+            }
+
+            switch( state.getState() ) {
+                case PlaybackStateCompat.STATE_PLAYING: {
+                    mCurrentState = PlaybackStateCompat.STATE_PLAYING;
+                    break;
+                }
+                case PlaybackStateCompat.STATE_PAUSED: {
+                    mCurrentState = PlaybackStateCompat.STATE_PAUSED;
+                    break;
+                }
+            }
+        }
+    };
 
     // Spotify Objects
     UserPrivate mPrivateUser;
@@ -55,7 +152,11 @@ public class SpotifyManager implements SpotifyPlayer.NotificationCallback, Conne
     //Services
     RefreshSpotifyAccessTokenService refreshSpotifyAccessTokenService;
 
-    private SpotifyManager() {
+    public SpotifyManager() {
+        super.onCreate();
+        initMediaPlayer();
+        initMediaSession();
+        initNoisyReceiver();
         final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
         executor.scheduleAtFixedRate(new Runnable() {
             @Override
@@ -71,6 +172,154 @@ public class SpotifyManager implements SpotifyPlayer.NotificationCallback, Conne
         }, 55, 55, TimeUnit.MINUTES); // just less than an hour (token expires every hour)
     }
 
+    private void initMediaPlayer() {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setVolume(1.0f, 1.0f);
+    }
+
+    private void initMediaSession() {
+        ComponentName mediaButtonReceiver = new ComponentName(getApplicationContext(), MediaButtonReceiver.class);
+        mMediaSessionCompat = new MediaSessionCompat(getApplicationContext(), "Tag", mediaButtonReceiver, null);
+
+        mMediaSessionCompat.setCallback(mMediaSessionCallback);
+        mMediaSessionCompat.setFlags( MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS );
+
+        Intent mediaButtonIntent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        mediaButtonIntent.setClass(this, MediaButtonReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, mediaButtonIntent, 0);
+        mMediaSessionCompat.setMediaButtonReceiver(pendingIntent);
+
+        setSessionToken(mMediaSessionCompat.getSessionToken());
+    }
+
+    private void setMediaPlaybackState(int state) {
+        PlaybackStateCompat.Builder playbackstateBuilder = new PlaybackStateCompat.Builder();
+        if( state == PlaybackStateCompat.STATE_PLAYING ) {
+            playbackstateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_PAUSE);
+        } else {
+            playbackstateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_PLAY);
+        }
+        playbackstateBuilder.setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 0);
+        mMediaSessionCompat.setPlaybackState(playbackstateBuilder.build());
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        if( mMediaPlayer != null ) {
+            // TODO: instead play next song here
+            mMediaPlayer.release();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.abandonAudioFocus(this);
+        unregisterReceiver(mNoisyReceiver);
+        mMediaSessionCompat.release();
+        NotificationManagerCompat.from(this).cancel(1);
+    }
+
+    private void showPausedNotification() {
+        NotificationCompat.Builder builder = MediaStyleHelper.from(this, mMediaSessionCompat);
+        if( builder == null ) {
+            return;
+        }
+
+        builder.addAction(new NotificationCompat.Action(android.R.drawable.ic_media_play, "Play", MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE)));
+        builder.setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle().setShowActionsInCompactView(0).setMediaSession(mMediaSessionCompat.getSessionToken()));
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat.from(this).notify(1, builder.build());
+    }
+
+    private void showPlayingNotification() {
+        NotificationCompat.Builder builder = MediaStyleHelper.from(SpotifyManager.this, mMediaSessionCompat);
+        if( builder == null ) {
+            return;
+        }
+
+
+        builder.addAction(new NotificationCompat.Action(android.R.drawable.ic_media_pause, "Pause", MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE)));
+        builder.setStyle(new NotificationCompat.MediaStyle().setShowActionsInCompactView(0).setMediaSession(mMediaSessionCompat.getSessionToken()));
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat.from(SpotifyManager.this).notify(1, builder.build());
+    }
+
+    private void initMediaSessionMetadata() {
+        MediaMetadataCompat.Builder metadataBuilder = new MediaMetadataCompat.Builder();
+        //Notification icon in card
+        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_DISPLAY_ICON, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+
+        //lock screen icon for pre lollipop
+        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ART, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, "Display Title");
+        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_SUBTITLE, "Display Subtitle");
+        metadataBuilder.putLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER, 1);
+        metadataBuilder.putLong(MediaMetadataCompat.METADATA_KEY_NUM_TRACKS, 1);
+
+        mMediaSessionCompat.setMetadata(metadataBuilder.build());
+    }
+
+    private void initNoisyReceiver() {
+        //Handles headphones coming unplugged. cannot be done through a manifest receiver
+        IntentFilter filter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
+        registerReceiver(mNoisyReceiver, filter);
+    }
+
+    private boolean successfullyRetrievedAudioFocus() {
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        int result = audioManager.requestAudioFocus(this,
+                AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
+        return result == AudioManager.AUDIOFOCUS_GAIN;
+    }
+
+//    public void playUri(Player.OperationCallback o, String uri, int i1, int i2){
+//        mCurrentUri = uri;
+//
+//    }
+
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+        switch( focusChange ) {
+            case AudioManager.AUDIOFOCUS_LOSS: {
+                if( mMediaPlayer.isPlaying() ) {
+                    mMediaPlayer.stop();
+                }
+                break;
+            }
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT: {
+                mMediaPlayer.pause();
+                break;
+            }
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK: {
+                if( mMediaPlayer != null ) {
+                    mMediaPlayer.setVolume(0.3f, 0.3f);
+                }
+                break;
+            }
+            case AudioManager.AUDIOFOCUS_GAIN: {
+                if( mMediaPlayer != null ) {
+                    if( !mMediaPlayer.isPlaying() ) {
+                        mMediaPlayer.start();
+                    }
+                    mMediaPlayer.setVolume(1.0f, 1.0f);
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        MediaButtonReceiver.handleIntent(mMediaSessionCompat, intent);
+        return super.onStartCommand(intent, flags, startId);
+    }
 
     public static SpotifyManager getInstance() {
         if (baseActivity == null) { // this means we haven't logged in so you don't get one
@@ -261,5 +510,21 @@ public class SpotifyManager implements SpotifyPlayer.NotificationCallback, Conne
     @Override
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Nullable
+    @Override
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
+        if(TextUtils.equals(clientPackageName, getPackageName())) {
+            return new BrowserRoot(getString(R.string.app_name), null);
+        }
+
+        return null;
+    }
+
+    //Not important for general audio service, required for class
+    @Override
+    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+        result.sendResult(null);
     }
 }
